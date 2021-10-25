59079ce847a7f17a5e7eddf2bb1bf9693dca1b1f
Yura Yura <yuriydmytrukr@gmail.com>
2021-10-25 14:24:48 +0300

    a

--- DataManager/Managers/StatusManager.cs
+++ DataManager/Managers/StatusManager.cs
@@ -5,6 +5,7 @@ namespace DataManager
 {
     class StatusManager
     {
+
         public static StatusModel GetbyId(int id)
         {
             StatusModel status = ApplicationContextHolder.context.Status.FirstOrDefault(s => s.Id == id);
