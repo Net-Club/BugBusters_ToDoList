cb67c10fa22d9f9a50afa6b5380e5ae5e8c8296b
Yura Yura <yuriydmytrukr@gmail.com>
2021-10-26 12:35:27 +0300

    Add all controlers

--- Models/TaskStatusModel.cs
+++ Models/TaskStatusModel.cs
@@ -2,23 +2,21 @@
 {
     public class TaskStatusModel
     {
-        private TaskModel task;
-        private StatusModel status;
-
-        public TaskModel Task{
-            get { return task; }
-            set { task = value; }
-        }
-
-        public StatusModel Status {
-            get { return status; }
-            set { status = value; }
-        }
+        private int id;
+        private string task;
+        private string description;
+        private int user_id;
+        private string status;
+        private string status_desc;
 
         public TaskStatusModel(TaskModel task, StatusModel status) 
         {
-            this.task = task;
-            this.status = status;
+            id = task.Id;
+            this.task = task.Task;
+            description = task.Description;
+            user_id = task.User_id;
+            this.status = status.Status;
+            status_desc = status.Description;
         }
     }
 }
--- ResourceServer/Controllers/TaskController.cs
+++ ResourceServer/Controllers/TaskController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataManager;
+using Microsoft.AspNetCore.Mvc;
 using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text.Json;
 
 namespace ResourceServer.Controllers
 {
@@ -11,30 +13,72 @@ namespace ResourceServer.Controllers
     public class TaskController : Controller
     {
         private int UserId => Int32.Parse(User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier).Value);
-        private string UserName => User.Claims.Single(c => c.Type == ClaimTypes.Email).Value;
 
         [HttpGet]
         public ReturnModel<TaskStatusModel> Get()
         {
-            return null;
+            List<TaskStatusModel> data = DataManager.Utill.GetTaskStatusList(UserId);
+
+            if (data.Count == 0) { return new ReturnModel<TaskStatusModel>(null, 400, "Nothing was found"); }
+            return new ReturnModel<TaskStatusModel>(data, 200, "All tasks returned");
         }
 
         [HttpPost]
-        public ReturnModel<string> Post()
+        public ReturnModel<string> Post([FromBody] JsonElement JSdata)
         {
-            return null;
+            try
+            {
+                TaskModel task = new TaskModel(
+                    JSdata.GetProperty("task").GetString(),
+                    JSdata.GetProperty("description").GetString(),
+                    JSdata.GetProperty("user_id").GetInt32(),
+                    JSdata.GetProperty("status_id").GetInt32()
+                    );
+
+                if (TaskManager.Post(task)) { return new ReturnModel<string>(null, 200, "Data saved"); }
+                return new ReturnModel<string>(null, 400, "Data not saved");
+            }
+            catch
+            {
+                return new ReturnModel<string>(null, 400, "Wrong JS Data");
+            }
         }
 
         [HttpPut]
-        public ReturnModel<string> Put()
+        public ReturnModel<string> Put([FromBody] JsonElement JSdata)
         {
-            return null;
+            try
+            {
+                TaskModel task = new TaskModel(
+                    JSdata.GetProperty("task").GetString(),
+                    JSdata.GetProperty("description").GetString(),
+                    JSdata.GetProperty("user_id").GetInt32(),
+                    JSdata.GetProperty("status_id").GetInt32()
+                    );
+
+                if (TaskManager.Put(task)) { return new ReturnModel<string>(null, 200, "Data updated"); }
+                return new ReturnModel<string>(null, 400, "Data not updated");
+            }
+            catch
+            {
+                return new ReturnModel<string>(null, 400, "Wrong JS Data");
+            }
         }
 
         [HttpDelete]
-        public ReturnModel<string> Delete()
+        public ReturnModel<string> Delete([FromBody] JsonElement JSdata)
         {
-            return null;
+            try
+            {
+                int id = JSdata.GetProperty("id").GetInt32();
+
+                if (TaskManager.Delete(id)) { return new ReturnModel<string>(null, 200, "Data deleted"); }
+                return new ReturnModel<string>(null, 400, "Data not deleted");
+            }
+            catch
+            {
+                return new ReturnModel<string>(null, 400, "Wrong JS Data");
+            }
         }
     }
 }
