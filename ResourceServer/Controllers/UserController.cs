17415a6d71b991d2053585ec938e3d8c045f5c61
Yura Yura <yuriydmytrukr@gmail.com>
2021-10-26 12:43:25 +0300

    Delete sime o reference functions and not used includes

--- ResourceServer/Controllers/UserController.cs
+++ ResourceServer/Controllers/UserController.cs
@@ -1,11 +1,7 @@
 ﻿using DataManager;
 using Microsoft.AspNetCore.Mvc;
 using Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace ResourceServer.Controllers
 {
