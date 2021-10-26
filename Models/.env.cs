36de50173c49bf42bd3d8ad28a115a74d575a70f
Yura Yura <yuriydmytrukr@gmail.com>
2021-10-26 17:40:49 +0300

    Add env

--- Models/.env.cs
+++ Models/.env.cs
@@ -2,7 +2,8 @@
 {
     public class env
     {
-        private const string _connectionString = "Server=localhost; Port=5432; User Id=postgres; Password=Dima2001; Database=ToDoTasks";
+        private const string _connectionString = "Server=localhost; Port=5432; User Id=postgres; Password=1111; Database=ToDoList";
+        //private const string _connectionString = "Server=localhost; Port=5432; User Id=postgres; Password=Dima2001; Database=ToDoTasks";
         public static string GetConfigurationString()
         {
             return _connectionString;
