aae92f13303333e4a158493b061fad883ecd2af7
Yura Yura <yuriydmytrukr@gmail.com>
2021-10-26 17:32:12 +0300

    Edit Models

--- Models/TaskStatusModel.cs
+++ Models/TaskStatusModel.cs
@@ -9,6 +9,38 @@
         private string status;
         private string status_desc;
 
+        public int Id 
+        {
+            get  { return id; }
+            set { id = value; }
+        }
+        public int User_id
+        {
+            get { return user_id; }
+            set { user_id = value; }
+        }
+        public string Task
+        {
+            get { return task; }
+            set { task = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+        public string Status_desc
+        {
+            get { return status_desc; }
+            set { status_desc = value; }
+        }
+
+
         public TaskStatusModel(TaskModel task, StatusModel status) 
         {
             id = task.Id;
