1a55526d4f0025523719ca6cfc5d940c121199f9
Yura Yura <yuriydmytrukr@gmail.com>
2021-10-27 17:09:13 +0300

    Add RegisterComponent

--- ClientServer/client_server/src/Components/RegisterComponent/RegisterComponent.tsx
+++ ClientServer/client_server/src/Components/RegisterComponent/RegisterComponent.tsx
@@ -3,7 +3,17 @@ import React from 'react';
 
 function Register() {
   return (
-    <h1>Register</h1>
+    <form>
+  
+    <h1 className="h3 mb-3 fw-normal, header">Please register</h1>
+
+    <input  type="name" className="form-control" placeholder="Name" required/>
+
+    <input  type="password" className="form-control" placeholder="Password" required/>
+
+    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+
+  </form>
   );
 }
 
