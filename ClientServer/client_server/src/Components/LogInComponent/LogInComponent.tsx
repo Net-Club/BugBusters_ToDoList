c5f98bc9ce507dda00f6b8ab7c3745d9ddf2479f
Yura Yura <yuriydmytrukr@gmail.com>
2021-10-27 19:01:14 +0300

    LogIn

--- ClientServer/client_server/src/Components/LogInComponent/LogInComponent.tsx
+++ ClientServer/client_server/src/Components/LogInComponent/LogInComponent.tsx
@@ -1,21 +1,29 @@
+import { allowedNodeEnvironmentFlags } from 'process';
 import React from 'react';
 import './LogIn.css'
 
-
 function LogIn() {
   return (
-    <form>
+    <form >
   
     <h1 className="h3 mb-3 fw-normal, header">Please sign in</h1>
 
-    <input  type="name" className="form-control" placeholder="Name" required/>
+    <input id="name"  type="name" className="form-control" placeholder="Name" required/>
 
-    <input  type="password" className="form-control" placeholder="Password" required/>
+    <input id="password" type="password" className="form-control" placeholder="Password" required/>
 
-    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={_logIn}>Sign in</button>
 
   </form>
   );
+}
+
+function _logIn()
+{
+  let name = document.getElementById('name')?.textContent;
+  let password = document.getElementById('password')?.textContent;
+
+  console.log("name", "password");
 }
 
 export default LogIn;
