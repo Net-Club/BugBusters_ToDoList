57c1c3f2841f633a0aa822d56294314b51bc4367
Dima Golovaci <herous2001@gmail.com>
2021-10-29 13:07:13 +0300

    getting answer

---
+++ ClientServer/client_server/src/Models/ReturnModel.ts
@@ -0,0 +1,17 @@
+export class ReturnModel{
+    public data!: Array<any>;
+    public status!: number;
+    public message!: string;
+
+    constructor(data:Array<any>, status:number, message:string)
+    {
+        this.data = data;
+        this.status = status;
+        this.message = message;
+    }
+
+    public setMessage(message: string){
+        this.message = message;
+    }
+}
+
