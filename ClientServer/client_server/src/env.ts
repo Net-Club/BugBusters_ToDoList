69bb3e22a9701e16aadab4846be7b76acb8b489a
Yura Yura <yuriydmytrukr@gmail.com>
2021-11-02 15:21:39 +0200

    Clear from ;

--- ClientServer/client_server/src/env.ts
+++ ClientServer/client_server/src/env.ts
@@ -2,11 +2,11 @@ export class environment
 {
     public static GetResUrl(end:string) : string
     {
-        return "http://localhost:43073/res" + end;
+        return "http://localhost:43073/res" + end
     }
 
     public static GetAuthUrl(end:string) : string
     {
-        return "http://localhost:32446/auth" + end;
+        return "http://localhost:32446/auth" + end
     }
 }
